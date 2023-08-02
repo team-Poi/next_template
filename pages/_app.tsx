ac5d6c7a90c6bb6aac47c58bbccceef081c16d6e
Oein <oein0219@gmail.com>
2023-08-02 14:08:55 +0900

    add ui and toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@team.poi/ui/src/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import type { AppProps } from "next/app";
+import { ModalProvider } from "@team.poi/ui/dist/cjs/hooks/Modal";
+import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ModalProvider>
+      <Analytics />
+      <ToastContainer autoClose={3000} position="bottom-right" />
+      <Component {...pageProps} />
+    </ModalProvider>
+  );
 }
